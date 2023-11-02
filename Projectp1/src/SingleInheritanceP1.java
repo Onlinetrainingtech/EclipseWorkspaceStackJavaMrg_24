b7d1a5b16cd7e7bceca5db36ebaea9b1dc480a7e
azartechnet <azartechnet@gmail.com>
2023-11-02 18:37:54 +0530

    updated

---
+++ Projectp1/src/SingleInheritanceP1.java
@@ -0,0 +1,27 @@
+class B
+{
+	int productid=1001;
+	String pname="apple";
+	void get1()
+	{
+		System.out.println("Your Base class value is::"+productid);
+	}
+}
+class D extends B
+{
+	void get2()
+	{
+	   System.out.println("Your Derived class value is::"+pname);	
+	}
+}
+public class SingleInheritanceP1 {
+
+	public static void main(String[] args) {
+		
+		D f1=new D();
+		f1.get1();
+		f1.get2();
+
+	}
+
+}
--- Projectp1/src/StringP1.java
+++ Projectp1/src/StringP1.java
@@ -10,6 +10,11 @@ public class StringP1 {
 		System.out.println(s2.length());
 		System.out.println(s2.charAt(2));
 	    System.out.println(s1.equals(s2));
+	    System.out.println(s1.startsWith("a"));
+	    System.out.println(s1.endsWith("n"));
+	    System.out.println(s1.compareTo(s2));
+	    System.out.println(s1.toUpperCase());
+	    System.out.println(s1.toLowerCase());
 
 	}
 
