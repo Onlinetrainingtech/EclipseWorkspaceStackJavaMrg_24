c9c598e051e7c0adda804b1f096a4af170264e92
azartechnet <azartechnet@gmail.com>
2023-12-12 22:36:59 +0530

    updated

---
+++ Projectp1/src/ThreadP1.java
@@ -0,0 +1,34 @@
+class Demo15 extends Thread
+{
+	public void run()
+	{
+		try
+		{
+			System.out.println("ThreadIn");
+			Thread.sleep(10000);
+			System.out.println("ThreadOut"+currentThread().getPriority());
+		}
+		catch(InterruptedException t)
+		{
+			
+		}
+//		System.out.println("This is Thread Running..");
+	}
+}
+public class ThreadP1 {
+
+	public static void main(String[] args) {
+		
+		Demo15 t1=new Demo15();//Thread-0
+		Demo15 t2=new Demo15();//Thread-1
+		t1.start();
+		t1.setName("ThreadWelcome");
+		t1.setPriority(Thread.MIN_PRIORITY);
+		System.out.println(t1.getName());
+		t2.start();
+		t2.setPriority(Thread.MAX_PRIORITY);
+		System.out.println(t2.getName());
+
+	}
+
+}
